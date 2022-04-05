9fe4dd0cda87d8aecdf53fddad063afc1842e11f
todorkermedchiev <todorkerm@gmail.com>
2022-04-05 16:51:43 +0300

    Test the program

--- Week5/Timestamp/main.cpp
+++ Week5/Timestamp/main.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main() {
     Timestamp time(23, 59, 59);
-    /*long seconds = 0;
-    cin >> seconds;
-    time.secondsToTime(seconds);*/
 
-    // time.setMinutes(15);
-    // time.addSeconds(65);
-    // time.addMinutes(12);
-    // cout << time.getHours() << endl;
+    time.setMinutes(15);
+    time.addSeconds(65);
+    time.addMinutes(12);
+    cout << time.getHours() << endl;
     time.print();
+
+    return 0;
 }
